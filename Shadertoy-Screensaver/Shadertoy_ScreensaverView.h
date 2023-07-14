e686ac1847541c2126a927bc3ae55ccd4d76c1a9
Lauri Saikkonen <lauri.saikkonen@nitor.com>
2023-07-14 18:00:26 +0300

    Initial Commit

---
+++ Shadertoy-Screensaver/Shadertoy_ScreensaverView.h
@@ -0,0 +1,12 @@
+//
+//  Shadertoy_ScreensaverView.h
+//  Shadertoy-Screensaver
+//
+//  Created by Lauri Saikkonen on 14.7.2023.
+//
+
+#import <ScreenSaver/ScreenSaver.h>
+
+@interface Shadertoy_ScreensaverView : ScreenSaverView
+
+@end
---
+++ Shadertoy-Screensaver/Shadertoy_ScreensaverView.m
@@ -0,0 +1,51 @@
+//
+//  Shadertoy_ScreensaverView.m
+//  Shadertoy-Screensaver
+//
+//  Created by Lauri Saikkonen on 14.7.2023.
+//
+
+#import "Shadertoy_ScreensaverView.h"
+
+@implementation Shadertoy_ScreensaverView
+
+- (instancetype)initWithFrame:(NSRect)frame isPreview:(BOOL)isPreview
+{
+    self = [super initWithFrame:frame isPreview:isPreview];
+    if (self) {
+        [self setAnimationTimeInterval:1/30.0];
+    }
+    return self;
+}
+
+- (void)startAnimation
+{
+    [super startAnimation];
+}
+
+- (void)stopAnimation
+{
+    [super stopAnimation];
+}
+
+- (void)drawRect:(NSRect)rect
+{
+    [super drawRect:rect];
+}
+
+- (void)animateOneFrame
+{
+    return;
+}
+
+- (BOOL)hasConfigureSheet
+{
+    return NO;
+}
+
+- (NSWindow*)configureSheet
+{
+    return nil;
+}
+
+@end
