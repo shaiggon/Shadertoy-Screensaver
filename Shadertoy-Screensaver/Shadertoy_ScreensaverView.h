522d92bb46d408e9a7cbd415b61e197634376bee
Lauri Saikkonen <lauri.saikkonen@nitor.com>
2023-07-25 15:32:16 +0300

    Remove unused variables

--- Shadertoy-Screensaver/Shadertoy_ScreensaverView.h
+++ Shadertoy-Screensaver/Shadertoy_ScreensaverView.h
@@ -9,10 +9,6 @@
 
 @interface Shadertoy_ScreensaverView : ScreenSaverView
 
-@property (strong) NSColor *myColor;
-@property (strong) NSBezierPath *myPath;
-@property BOOL shouldFill;
-@property (strong) NSImage *previousFrame;
 @property NSOpenGLContext *openGLContext;
 @property GLuint program;
 @property double time;
